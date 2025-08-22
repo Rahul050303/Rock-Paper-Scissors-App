f4647b8b165bc075639e5c4cc6e4c963363bd644
Rahul050303 <123380259+Rahul050303@users.noreply.github.com>
2025-08-22 19:20:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay(playerMove){
+    if(!isAutoPlaying){
+        intervalId = setInterval(function(){
+            const playerMove = pickComputerMove();
+            play(playerMove);
+        },1000);
+        isAutoPlaying = true;
+    }else {
+        clearInterval(intervalId);
+        isAutoPlaying = false;
+    }
+}
+
+function autoPlayButton(){
+    const autoPlayElement = document.querySelector('.js-auto-play');
+    if(autoPlayElement.textContent === 'Auto Play'){
+        autoPlayElement.textContent = 'Stop Auto Play';
+    }else{
+        autoPlayElement.textContent = 'Auto Play';
+    }
+}
+
 let score = JSON.parse(localStorage.getItem('score')) || {
     wins : 0,
     losses : 0,
@@ -6,10 +31,16 @@ let score = JSON.parse(localStorage.getItem('score')) || {
 
 function updateScoreBoard() {
     document.getElementById('scoreDisplay').textContent =
-        `Wins : ${score.wins} , Losses : ${score.losses} , Ties : ${score.ties}`;
+    `Wins : ${score.wins} , Losses : ${score.losses} , Ties : ${score.ties}`;
 }
 
 updateScoreBoard();
+
+function resetScore(){
+    score = { wins: 0, losses: 0, ties: 0 };
+    localStorage.removeItem('score');
+    updateScoreBoard();
+}
 
 function play(playerMove){
     const computerMove = pickComputerMove();
@@ -40,7 +71,7 @@ function play(playerMove){
             result = 'tie';
         }
     }
-
+    
     if(result === 'win'){
         score.wins++;
     }else if(result === 'lose'){
@@ -48,16 +79,16 @@ function play(playerMove){
     }else {
         score.ties++;
     }
-
+    
     localStorage.setItem('score', JSON.stringify(score));
     updateScoreBoard();
-
+    
     showResult(result,computerMove,playerMove);
 }
 
 function showResult(result, computerMove, playerMove){
     const resultElement = document.querySelector('.js-result');
-
+    
     const images = {
         rock: "https://nehalhazem.github.io/rockPaperScissors.io/img/rock.png",
         paper: "https://sihoonathan.github.io/rock-paper-scissors/assets/img/paper.png",
@@ -78,7 +109,6 @@ function showResult(result, computerMove, playerMove){
     `;
 }
 
-
 function pickComputerMove(){
     const randomNumber = Math.random();
     let computerMove = '';
@@ -91,10 +121,4 @@ function pickComputerMove(){
         computerMove = 'scissors';
     }
     return computerMove;
-}
-
-function resetScore(){
-    score = { wins: 0, losses: 0, ties: 0 };
-    localStorage.removeItem('score');
-    updateScoreBoard();
 }
